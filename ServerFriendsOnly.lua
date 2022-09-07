02263722242943dc64cbb1416fd4a10236af1889
Nitrak <nitrak@wow.com>
2022-09-07 19:01:45 +0200

    Packable for WotLK and Classic

--- ServerFriendsOnly.lua
+++ ServerFriendsOnly.lua
@@ -55,6 +55,52 @@ local lastClicked = ""
 local friendsTable = {}
 local friendsTableLength = 0
 
+local intendedWoWProject = WOW_PROJECT_CLASSIC
+
+--[===[@non-version-retail@
+--@version-classic@
+intendedWoWProject = WOW_PROJECT_CLASSIC
+--@end-version-classic@
+--@version-wrath@
+intendedWoWProject = WOW_PROJECT_WRATH_CLASSIC
+--@end-version-wrath@
+--@end-non-version-retail@]===]
+
+local function IsClassic()
+  return WOW_PROJECT_ID == WOW_PROJECT_CLASSIC
+end
+
+local function IsWrath()
+	return WOW_PROJECT_ID == WOW_PROJECT_WRATH_CLASSIC
+end
+
+local function IsRetail()
+  return WOW_PROJECT_ID == WOW_PROJECT_MAINLINE
+end
+
+local function IsCorrectVersion()
+  return intendedWoWProject == WOW_PROJECT_ID
+end
+
+local function prettyPrint(...)
+  print("|c00ff0000Server Friends Only:|r ", ...)
+end
+
+local intendedWoWProjectName = {
+  [WOW_PROJECT_MAINLINE] = "Retail",
+  [WOW_PROJECT_CLASSIC] = "Classic",
+  [WOW_PROJECT_WRATH_CLASSIC or 11] = "Wrath of the Lich King Classic"
+}
+
+local wrongTargetMessage = "This version of Server Friends Only was packaged for World of Warcraft " .. intendedWoWProjectName[intendedWoWProject] ..
+                              ". Please install the " .. intendedWoWProjectName[WOW_PROJECT_ID] ..
+                              " version instead.\nIf you are using an addon manager, then" ..
+                              " contact their support for further assistance and reinstall SFO manually."
+
+if not IsCorrectVersion() then --Wait 10 seconds then error message
+  C_Timer.After(10, function() prettyPrint(wrongTargetMessage) end)
+end
+
 --https://wowwiki.fandom.com/wiki/API_EasyMenu
 
 local function HideTab()
